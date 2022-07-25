85f1e03633a623ca40d233fa4e88c13b7138e1b5
Tuấn Lê <tuanle37372000@gmail.com>
2022-07-25 23:58:52 +0700

    add

--- client/App.js
+++ client/App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigators/StackNavigator'
